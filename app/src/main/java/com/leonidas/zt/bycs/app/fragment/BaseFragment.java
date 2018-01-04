8bc3918bd34605ca1d03515d0db2a1c99d47e496
mebee <hd_iot@163.com>
2018-01-04 10:44:51 +0800

    分包

--- app/src/main/java/com/leonidas/zt/bycs/app/fragment/BaseFragment.java
+++ app/src/main/java/com/leonidas/zt/bycs/app/fragment/BaseFragment.java
@@ -4,6 +4,10 @@ import android.support.v4.app.Fragment;
 
 /**
  * Created by mebee on 2018/1/2.
+ * Version: V1.0
+ * Description:
+ * Others: 暂无
+ * ReviseHistory(Author、Date、RevisePart)： 暂无
  */
 
 public class BaseFragment extends Fragment {
